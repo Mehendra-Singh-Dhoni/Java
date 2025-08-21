985cd80bb0283c07a343a32527cd455818a4656d
Mehendra-Singh-Dhoni <sohamrokade1239@gmail.com>
2025-08-21 11:10:50 +0000

    21/08

---
+++ Amstrong.java
@@ -0,0 +1,19 @@
+public class Amstrong{
+    public static void main(String args[]){
+        int sum=0;
+        int a=153;
+        int check=a;
+        while(a>0){
+            int b=(a%10);
+            int c=(b*b*b);
+            sum+=c;
+            a=a/10;
+        }
+        if (sum==check){
+            System.out.println(check + " is an amstrong number");
+        }
+        else{
+                System.out.println(check + " is not an amstrong number");
+        }
+    }
+}
