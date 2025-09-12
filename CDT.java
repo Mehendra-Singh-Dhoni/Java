8d63e5d83fa8555da1d6ab238a697781c3ac5795
Mehendra-Singh-Dhoni <sohamrokade1239@gmail.com>
2025-09-12 13:16:26 +0000

    12-09

---
+++ CDT.java
@@ -0,0 +1,7 @@
+import java.time.LocalDateTime;
+public class CDT{
+    public static void main(String args[]){
+    LocalDateTime cur= LocalDateTime.now();
+    System.out.println("Current time: " + cur);
+}
+}
